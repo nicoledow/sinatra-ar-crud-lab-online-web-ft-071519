d413a852a588badacb28ec720c08015ba93f8480
Nicole Dow <nikki.dow71@gmail.com>
2019-08-30 14:12:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,45 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
 
   get '/' do
   end
+  
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show_article
+  end
+  
+  post '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    @article.update(content: params["edit_article_content"])
+    redirect "/articles/#{@article.id}"
+  end
+  
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :show
+  end
+  
+  
+  post '/articles' do
+    @new_article = Article.new({:title => params["title"], :content => params["content"]})
+    @new_article.save
+    redirect "/articles/#{@new_article.id}"
+  end
+  
 end
