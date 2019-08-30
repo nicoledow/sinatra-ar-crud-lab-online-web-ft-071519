02bead4f492f8cc29c2ef4532fae36c1e7ce8f41
Nicole Dow <nikki.dow71@gmail.com>
2019-08-30 14:32:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,19 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
   
+  #   # edit
+  # get "/articles/:id/edit" do
+  #   @article = Article.find(params[:id])
+  #   erb :edit
+  # end
+
+  # # update
+  # patch "/articles/:id" do
+  #   @article = Article.find(params[:id])
+  #   @article.update(params[:article])
+  #   redirect to "/articles/#{ @article.id }"
+  # end
+  
   get '/articles/:id/edit' do
     @article = Article.find(params[:id])
     erb :edit
