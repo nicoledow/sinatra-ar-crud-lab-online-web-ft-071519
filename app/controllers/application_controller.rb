a67d8dc3e231805db0d2e71f341ff49161142ede
Nicole Dow <nikki.dow71@gmail.com>
2019-08-30 15:15:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,53 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
 
   get '/' do
+    redirect to "/articles"
   end
+  
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+   # binding.pry
+    @article.update(:title => params["edit_article_title"], :content => params["edit_article_content"])
+    redirect to "/articles/#{@article.id}"
+  end
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show_article
+  end
+  
+  delete '/articles/:id' do
+    @article_to_delete = Article.find(params["id"])
+    @article_to_delete.destroy
+    redirect to "/articles"
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :show
+  end
+  
+  
+  post '/articles' do
+    @new_article = Article.new({:title => params["title"], :content => params["content"]})
+    @new_article.save
+    redirect "/articles/#{@new_article.id}"
+  end
+  
+
+  
 end
